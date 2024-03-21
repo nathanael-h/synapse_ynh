aeba4c40a70e699def60d21989144518b75d4f16
Josué Tille <josue@tille.ch>
2024-03-21 23:34:04 +0100

    fix coturn rotate script

--- sources/Coturn_config_rotate.sh
+++ sources/Coturn_config_rotate.sh
@@ -3,6 +3,7 @@
 set -eu
 
 app=__APP__
+YNH_APP_BASEDIR=/etc/yunohost/apps/"$app"
 
 pushd /etc/yunohost/apps/$app/conf
 source /usr/share/yunohost/helpers
@@ -13,6 +14,7 @@ port_cli=$(ynh_app_setting_get --app=$app --key=port_cli)
 turnserver_pwd=$(ynh_app_setting_get --app=$app --key=turnserver_pwd)
 port_turnserver_tls=$(ynh_app_setting_get --app=$app --key=port_turnserver_tls)
 port_turnserver_alt_tls=$(ynh_app_setting_get --app=$app --key=port_turnserver_alt_tls)
+enable_dtls_for_audio_video_turn_call=$(ynh_app_setting_get --app=$app --key=enable_dtls_for_audio_video_turn_call)
 
 previous_checksum=$(ynh_app_setting_get --app=$app --key=checksum__etc_matrix-synapse_coturn.conf)
 configure_coturn
